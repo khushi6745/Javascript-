dcf4fa25270673c1bc884276dbcf762ac63e210f
Khushi Kumari <khushikumari151202@gmail.com>
2025-09-03 13:58:05 +0000

    Day 1: Completed variables

---
+++ basics/variables.js
@@ -0,0 +1,10 @@
+const accountId = 144553
+let accountEmail = "khushi@123"
+var accountPassword = "12345"
+
+accountCity = "delhi"
+accountEmail = "hc.@com"
+accountPassword = "0989"
+accountCity = "goa"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
