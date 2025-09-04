af3e3bf485c993bcc9be2bfb42b55a7eeab87eb7
Khushi Kumari <khushikumari151202@gmail.com>
2025-09-04 06:12:45 +0000

    Day2:Completed datatypes

---
+++ basics/02datatypes.js
@@ -0,0 +1,114 @@
+// let isLogged=false
+// let state;
+// let age=12
+
+// console.log(typeof null);//object
+// console.log(typeof undefined)
+
+// let score=33
+// let score=null//it becomes string as it is in double quotes
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+// let valueInNumber=Number(score)
+// // console.log(typeof valueInNumber);
+// console.log(valueInNumber)
+
+// let isLoggedIn="hitesh";
+
+// let booleanconvert=Boolean(isLoggedIn)
+// console.log(booleanconvert)
+
+//  let someNumber=33
+//  let stringNumber=String(someNumber)
+//  console.log(stringNumber)
+//  console.log(typeof stringNumber)
+
+
+///operations
+
+// let value=3;
+// let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/3)
+// console.log(2%3)
+
+
+// let str1="hello"
+// let str2="hitsh"
+// let str3=str1+str2;
+// console.log(str3)
+
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(2+2+"1")
+
+
+// console.log(+true)
+// console.log(+"")
+
+// let num1,num2,num3
+
+// num1=num2=num3=2+2;
+
+// let gameCounter=100;
+// // gameCounter++;//output=101;
+// ++gameCounter //output=101
+// console.log(gameCounter)
+
+// console.log("2">1)
+// console.log("02">1)
+
+// console.log(null>0)
+// console.log(null>=0)
+// console.log(null==0)
+
+// console.log(undefined>0)
+// console.log(undefined>=0)
+// console.log(undefined==0)
+
+// const id=Symbol('123')
+// const anotherid=Symbol('123')
+// console.log(id===anotherid)
+
+// const myfunction=function(){
+//     console.log("Hello WOrld");
+    
+// }
+// myfunction();
+// console.log(typeof myfunction);
+
+// const outsidetemp=null;
+// console.log(typeof outsidetemp);
+
+// let myfunction="google.com";
+// let otherfunction=myfunction
+
+// otherfunction="khushi@.com"
+
+// console.log(otherfunction)
+// console.log(myfunction);
+
+
+let userone={
+    email:"userone.com",
+    age:20
+}
+let usertwo=userone
+
+usertwo.email="usertwo.com"
+// console.log(userone);
+// console.log(usertwo);
+
+console.log(userone.email);
+console.log(usertwo.email);
+
+
+
