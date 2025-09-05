1e208cd92deb2311eea0b4c7350e2f6c5b65f896
Khushi Kumari <khushikumari151202@gmail.com>
2025-09-05 10:47:04 +0000

    Day 3: Practiced Strings

---
+++ basics/03string.js
@@ -0,0 +1,39 @@
+// const name="khushi"
+// const repo=80;
+
+// console.log(name+repo+"value");
+// console.log(`my name is ${name} and my repo is ${repo}`);
+
+
+const gameName=new String('hitesh-sh-hj')
+console.log(gameName[3]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('e'));
+// console.log(gameName.indexOf("e"))
+// console.log(gameName.indexOf(`e`));
+
+// const newString=gameName.substring(0,4)
+// console.log(newString);
+// console.log(gameName.substring(0,4));
+
+// const mystring=gameName.slice(-7,5);
+// console.log(mystring);
+
+// console.log(gameName.substring(-8,4));//even if we give negative index the substring didn't take it as from back it starts from default 0
+
+
+
+// const newString="    hitesh   "
+// console.log(newString);
+// console.log(newString.trim());
+
+const url="https://hitsheh%20.com/hiteshchaudhary"
+console.log(url.replace('%20','--'))
+
+console.log(url.includes('hello'))
+
+console.log(gameName.split("s"))
