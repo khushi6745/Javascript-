958722548fd2ee873ef0d938f207b4d6f636de54
Khushi Kumari <khushikumari151202@gmail.com>
2025-09-05 11:47:45 +0000

    Day 4: Practice of numbers and maths function

---
+++ basics/04numsandmaths.js
@@ -0,0 +1,39 @@
+// const score=400;
+// console.log(score);
+
+
+// const balance=new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2));
+
+// const otherNum=23.38
+// const num=otherNum.toPrecision(2)
+
+// console.log(num);
+
+// console.log(typeof otherNum)
+// console.log(typeof num);
+// const num1=1000;
+// console.log(num1.toLocaleString('en-IN'));
+
+/////////////////////////////////////////////////////MATHS
+// console.log(Math)
+// const num=88
+// console.log(Math.sqrt(num))
+// console.log(Math.abs(-44));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.2));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+
+// console.log(Math.random()*10+1);
+// console.log((Math.random()*10)+1);
+
+// console.log(Math.floor((Math.random()*10)+1));
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1))+min)
