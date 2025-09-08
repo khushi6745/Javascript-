2802cf17bc53c6589b08aa710d22899fda808e42
Khushi Kumari <khushikumari151202@gmail.com>
2025-09-08 17:36:54 +0000

    Day 6 practiced on basic of array

---
+++ Arrays/basicarray.js
@@ -0,0 +1,47 @@
+// const newarr=[1,2,3,4,5]
+// console.log(newarr);
+
+// const stringarr=["hello","my","name","is","khushi"]
+// console.log(stringarr);
+
+// const arr=new Array(1,2,4,3,4)
+// console.log(arr);
+// console.log(arr[2]);
+
+
+
+////ARRAY METHODS
+// const myarr=[3,5,8,10,4]
+// myarr.push(6)
+// myarr.push(29) //add element 29 at last 
+
+// myarr.pop() //remove last element from array
+
+// myarr.unshift(98) // add the given value at starting of the array
+
+// myarr.shift() //remove one element from the starting of the array
+// console.log(myarr);
+// console.log(myarr.includes(98))
+// console.log(myarr.indexOf(8));
+// console.log(myarr.indexOf(33));
+
+// const newarr=myarr.join()
+// console.log(myarr);
+// console.log(typeof newarr);
+
+
+const myarr=[3,4,8,26,98]
+
+console.log("A",myarr);
+
+const arr1=myarr.slice(1,3)
+
+console.log(arr1);
+console.log("B:",myarr);
+
+const arr2=myarr.splice(1,3)
+console.log("C",myarr);
+console.log(arr2);
+
+
+
